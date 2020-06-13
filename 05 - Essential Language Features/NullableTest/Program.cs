df401e82779c27dc1b47733ef92c91c1c4a59da7
Wagner Xu <wagnerhsu@hotmail.com>
2020-06-13 16:20:50 +0800

    Update dependency

---
+++ 05 - Essential Language Features/NullableTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NullableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = null;
+            Console.WriteLine("product:"+product);
+        }
+    }
+}
